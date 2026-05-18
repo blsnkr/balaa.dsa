94b1d4b0204efa482555dda9c08e4ce680f83baa
blsnkr <152837687+blsnkr@users.noreply.github.com>
2026-05-18 19:55:24 +0530

    Prob 1 : Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.Input:  2 Name yExpected Output:2Namey

---
+++ PS1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        String b = scan.next();
+        String c = scan.next();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
