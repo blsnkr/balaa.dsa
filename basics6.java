1e70bc42061c5702058555dd77c548a9b7a15c9b
blsnkr <152837687+blsnkr@users.noreply.github.com>
2026-05-19 19:38:29 +0530

    Create basics6.java

---
+++ basics6.java
@@ -0,0 +1,12 @@
+// You are using Java
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        
+      for(int i=a;i<=1000;i=i+a){
+        System.out.println(i);
+    }
+    }
+}
