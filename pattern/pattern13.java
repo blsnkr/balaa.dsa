3b008ebab78165a65c83b1ffa4ac3febad8b8295
blsnkr <152837687+blsnkr@users.noreply.github.com>
2026-05-20 00:00:50 +0530

    Create pattern13.java

---
+++ pattern/pattern13.java
@@ -0,0 +1,19 @@
+// You are using Java
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int n = scan.nextInt();
+       int count =1;
+       	for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(count+" ");
+				count++;
+				
+			}
+			
+			System.out.println();
+			
+		}
+    }
+}
