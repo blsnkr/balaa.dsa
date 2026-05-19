cad2947473854d4f628b9686c0c5c278dc361856
blsnkr <152837687+blsnkr@users.noreply.github.com>
2026-05-19 19:34:49 +0530

    Create basics5.java

---
+++ basics5.java
@@ -0,0 +1,13 @@
+// You are using Java
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        
+      for(int i=1;i<=b;i++){
+        System.out.println(a);
+    }
+    }
+}
